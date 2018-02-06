a0ef04323bb4d9f3b5af776a17cef928961475cb
Derik Nguyen <deriknguyen@Deriks-MacBook-Pro.local>
2018-02-05 23:59:58 -0800

    wow

--- data/process_reduce.py
+++ data/process_reduce.py
@@ -5,7 +5,11 @@ import sys
 #
 # This program simply represents the identity function.
 #
+# HELLLLOOOOO
 
 for line in sys.stdin:
-    sys.stdout.write(line)
+   pass #sys.stdout.write(line)
+
+for i in range(20):
+	sys.stdout.write('FinalRank:' +str(float(20 - i)) + '\t' +str(i) + '\n')
 
