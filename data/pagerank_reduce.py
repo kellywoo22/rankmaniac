321688ef99de2427ebc93b49e91d73b624e25d45
Kelly Woo <kellywoo@Kellys-MacBook-Air.local>
2018-02-08 07:21:59 -0800

    did i fix it now

--- data/pagerank_reduce.py
+++ data/pagerank_reduce.py
@@ -30,70 +30,21 @@ for line in infile:
         try:
             r = float(values[1]) # value is a float
         except ValueError:
+            # value is not a float, just reemit it
             sys.stdout.write(str(node)+'\t'+values[1]) 
             continue
-
+        # at this point the line must be an int, so it is the rank
         if node1 == node:
+        # if node same as previous, then keep adding rank
             p += r
         else:
+            # otherwise calculate the rank
             d = SCALE * p + 1. - SCALE
             p = r
-
-            # value is list of outlinks or previous rank, just emit it
+            # output the node and rank
             sys.stdout.write(str(node)+'\t'+str(d)+'\n')
+            # set new node
             node = node1
+# account for the last node
 d = SCALE * p + 1. - SCALE
 sys.stdout.write(str(node)+'\t'+str(d)+'\n')
-
-#    !/usr/bin/env python
-
-# import sys
-
-# d = {} # dictionary to hold node x, and PR(x)
-# SCALE = 0.85 # scale factor
-
-# first = sys.stdin.readline().split("\t")
-# # Let iteration key pass through
-# if first[0] == 'k':
-#     sys.stdout.write(line)
-# else:
-#     node = int(first[0]) # get node
-#     p = 0
-#     try:
-#         p = float(first[1]) # value is a float
-#     except ValueError:
-#         # value is not a float, just reemit it
-#         sys.stdout.write(str(node)+'\t'+first[1]) 
-
-# infile = sys.stdin
-# next(infile) # skip the first line, already counted
-
-# for line in infile:
-#     # Let iteration key pass through
-#     if line[0] == 'k':
-#         print "hello"
-#         print line
-#         sys.stdout.write(line)
-#         continue
-
-#     values = line.split("\t")
-#     node1 = int(values[0])
-#     r = 0
-#     try:
-#         r = float(values[1]) # value is a float
-#     except ValueError:
-#         # value is not a float, just reemit it
-#         sys.stdout.write(str(node)+'\t'+values[1]) 
-#         continue
-#     if node1 == node:
-#         p += r
-#         node = node1
-#     else:
-#         d[node] = SCALE * p + 1. - SCALE
-#         p = r
-#         node = node1
-
-# d[node] = p
-
-# for key in d:
-#     sys.stdout.write(str(key)+'\t'+str(d[key])+'\n')
